a70e2d9d9f710a1ff36b986fa3b071f7ad3f80c2
JoyKang210 <Joy.Kang@obigo.com>
2026-05-07 14:13:56 +0900

    feat: YouTube 서비스의 기본 인자에 사용자 에이전트 및 추출기 인자 추가

--- src/modules/youtube/youtube.service.ts
+++ src/modules/youtube/youtube.service.ts
@@ -353,7 +353,12 @@ export class YoutubeService {
   }
 
   private getBaseArgs(): string[] {
-    const args = ['--js-runtimes', 'node'];
+    const args = [
+      '--extractor-args',
+      'youtube:player_client=ios,android,web',
+      '--user-agent',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+    ];
     if (this.cookiesFilePath) {
       args.push('--cookies', this.cookiesFilePath);
     }
